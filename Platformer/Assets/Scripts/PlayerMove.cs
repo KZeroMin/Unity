10bfcdfb32045da388fd87d00ce3ed9561c03fa8
zeromin1410@gmail.com <zeromin1410@gmail.com>
2025-04-21 23:47:43 +0900

    Platform implementation with TileMap

--- Platformer/Assets/Scripts/PlayerMove.cs
+++ Platformer/Assets/Scripts/PlayerMove.cs
@@ -59,7 +59,7 @@ public class NewBehaviourScript : MonoBehaviour
         // Landing Platform
         if (rigid.velocity.y < 0)
         {
-            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
             if (rayHit.collider != null)
             {
